336c0e0c126f1c0a6c8118cf093ed357ee198147
Priyanka-dhake <priyanka@angularminds.in>
2021-10-27 09:59:33 +0530

    Swaglabs program

---
+++ runner.java
@@ -0,0 +1,18 @@
+package stepdefinitions;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/feature",glue= {"stepdefinitions"},
+monochrome =true,
+plugin= {"pretty","junit:target/Junitreports/reports.xml",
+		"json:target/JSONreports/reports.json",
+		"html:target/Htmlreports/reports.html"},
+tags= "@SmokeTest"
+)
+public class sawglabsrunner {
+
+}
